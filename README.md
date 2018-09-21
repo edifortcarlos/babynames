69b80fbd1822e659ce71ae4deadaf4537d12201c
Edilásio Fortunato Carlos <ecarlos.najareal@gmail.com>
2018-09-22 00:42:24 +0100

    'segundt'

---
+++ README.md
@@ -0,0 +1,8 @@
+# baby_names
+
+Aplicacao Flutter com Firebase
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Baby Names',
+      home: const MyHomePage(title: 'Vote o nome para o bebe'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
+    return new ListTile(
+      key: new ValueKey(document.documentID),
+      title: new Container(
+        decoration: new BoxDecoration(
+          border: new Border.all(color: const Color(0x80000000)),
+          borderRadius: new BorderRadius.circular(5.0),
+        ),
+        padding: const EdgeInsets.all(10.0),
+        child: new Row(
+          children: <Widget>[
+            new Expanded(
+              child: new Text(document['name']),
+            ),
+            new Text(
+              document['vote'].toString(),
+            ),
+          ],
+        ),
+      ),
+      onTap: () => Firestore.instance.runTransaction((transaction) async {
+        DocumentSnapshot freshSnap =
+        await transaction.get(document.reference);
+        await transaction.update(
+            freshSnap.reference, {'vote': freshSnap['vote'] + 1});
+      }),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text(title)),
+      body: new StreamBuilder(
+          stream: Firestore.instance.collection('bady').snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return const Text('Loading...');
+            return new ListView.builder(
+                itemCount: snapshot.data.documents.length,
+                padding: const EdgeInsets.only(top: 10.0),
+                itemExtent: 55.0,
+                itemBuilder: (context, index) =>
+                  _buildListItem(context, snapshot.data.documents[index]),
+
+            );
+          }),
+    );
+  }
+}
